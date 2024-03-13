86ebb75b85632adf249101f5dd99d85e91a4a352
Anti-RakPro <kvit.d.o@gmail.com>
2024-03-13 15:50:26 +0200

    init: add gitignore

---
+++ src/User.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var faker = require("faker");
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+console.log(faker);
