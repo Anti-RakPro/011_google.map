eaafa6e563bd307f9bd5169687aff5cc18efbfca
Anti-RakPro <kvit.d.o@gmail.com>
2024-03-18 12:55:11 +0200

    some test js

--- src/User.ts
+++ src/User.ts
@@ -6,11 +6,21 @@ class User {
         lat: number;
         lng: number;
     };
+    location2: {
+        lat: 15;
+        lng: 16;
+    };
 
     constructor() {
-
+        this.name = faker.name.firstName()
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
     }
 }
+
 console.log(faker)
-console.log('after faker')
+console.log(User.location)
+
 
---
+++ src/test.js
@@ -0,0 +1,9 @@
+class Test {
+    name: 'name',
+    constructor() {
+        this.age = 13
+    }
+
+}
+
+console.log(Test)
